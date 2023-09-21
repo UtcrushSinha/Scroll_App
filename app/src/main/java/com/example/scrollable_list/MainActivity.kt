1b1cad00816d0fffa1bbbd8ee741ed653910570f
Utkarsh SInha <vishalsingh@Vishals-MacBook-Air.local>
2023-09-21 16:42:45 +0530

    completed scroll down

--- app/src/main/java/com/example/scrollable_list/MainActivity.kt
+++ app/src/main/java/com/example/scrollable_list/MainActivity.kt
@@ -3,13 +3,28 @@ package com.example.scrollable_list
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.scrollable_list.data.Datasource
+import com.example.scrollable_list.model.Affirmation
 import com.example.scrollable_list.ui.theme.Scrollable_ListTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +37,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ScrollApp("Android")
+                    ScrollApp()
                 }
             }
         }
@@ -30,17 +45,62 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ScrollApp(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun ScrollApp() {
+
+    ScrollList(
+        affirmationList = Datasource().loadAffirmations(),
     )
+
+}
+
+@Composable
+fun ScrollList(affirmationList: List<Affirmation>, modifier: Modifier = Modifier) {
+
+    LazyColumn(modifier = modifier ){
+        items(affirmationList) { affirmation ->
+            ScrollCard(
+                affirmation = affirmation,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+
+    }
+
+}
+
+
+
+@Composable
+fun ScrollCard(affirmation: Affirmation,modifier: Modifier = Modifier){
+
+    Card(modifier = modifier) {
+        
+        Column() {
+            
+            Image(
+                painter = painterResource(id = affirmation.imageResourceId),
+                contentDescription = stringResource(id = affirmation.stringResourceId),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(194.dp),
+                contentScale = ContentScale.Crop)
+
+            Text(
+                text = LocalContext.current.getString(affirmation.stringResourceId),
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.headlineSmall
+            )
+            
+        }
+        
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Scrollable_ListTheme {
-        ScrollApp("Android")
+        ScrollApp()
     }
 }
---
+++ app/src/main/java/com/example/scrollable_list/model/Affirmation.kt
@@ -0,0 +1,14 @@
+package com.example.scrollable_list.model
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+
+
+
+
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+
+)
+
